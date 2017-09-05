be3d59c41f01087b7f1403a5e1e097404bab87e4
Dhayanithi <dhayanithikr12@gmail.com>
2017-09-05 20:38:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# python
+# Hackers
---
+++ stock.py
@@ -0,0 +1,23 @@
+'''
+This stock maintains the data to manipulate stock items with insert, delete, update and view modules
+'''
+def insertitems(list_of_items):
+	'''
+	This function adding details of a product items into the list
+	'''
+	print ('Insert Items Function')
+	print (list_of_items)
+
+def deleteitems(list_of_items):
+	print ('Delete Items Function')
+	print (list_of_items)
+
+def updateitems(list_of_items):
+	print ('Update Items Function')
+	print (list_of_items)
+
+def viewitems(list_of_items):
+	print ('View Items Function')
+	print (list_of_items)
+
+list_of_items = []
